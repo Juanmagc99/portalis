b7d84ea2bc00fa43517542a4ef2fe79687f3e3ed
Juanmagc99 <juanma101099@gmail.com>
2025-06-15 16:00:44 +0200

    small fix for instance model

--- portalis/internal/model/instance.go
+++ portalis/internal/model/instance.go
@@ -3,10 +3,10 @@ package model
 import "time"
 
 type Instance struct {
-	ServiceName string            `json:"serviceName validate:"required"`
+	ServiceName string            `json:"serviceName" validate:"required"`
 	InstanceID  string            `json:"instanceID" validate:"required"`
 	Host        string            `json:"host" validate:"required"`
-	Port        int               `json:"host" validate:"required"`
+	Port        int               `json:"port" validate:"required"`
 	Metadata    map[string]string `json:"metadata" validate:"required"`
 	LastSeen    time.Time         `json:"lastSeen" validate:"required"`
 }
